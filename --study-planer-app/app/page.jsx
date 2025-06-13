6728ea478a8eb1319483b13516cd0d3660255dd7
Mohammed Abdullah <m.abdulqader284@gmail.com>
2025-06-13 16:51:13 +0300

    .

--- --study-planer-app/app/page.jsx
+++ --study-planer-app/app/page.jsx
@@ -1,8 +1,6 @@
 "use client";
-import { Download, Upload } from "lucide-react";
 
-import Image from "next/image";
-import cuid from "cuid";
+
 import CreateCourseModal from "./components/CreateCourseModal.jsx";
 
 import React, { useEffect, useState } from "react";
